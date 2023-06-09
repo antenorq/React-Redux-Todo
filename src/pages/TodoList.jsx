d756a5886115e3c0e127d0dd1cce623d014a4663
antenorq <antenorq@gmail.com>
2023-06-09 17:08:31 -0400

    table

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTodos, deleteTodo } from "../slices/todoSlice";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Table } from "react-bootstrap";
 
 const TodoList = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos.items);
+  const loading = useSelector((state) => state.todos.loading);
+
+  console.log(todos);
+  console.log(loading);
 
   useEffect(() => {
     dispatch(getTodos());
@@ -17,6 +21,7 @@ const TodoList = () => {
 
   return (
     <div>
+      {loading ? "LOADING" : ""}
       {todos.map((todo, index) => (
         <Container key={index}>
           <Row>
@@ -29,6 +34,38 @@ const TodoList = () => {
             </Col>
           </Row>
         </Container>
+
+
+<thead>
+  <tr>
+    <th>ID</th>
+    <th>To Do</th>
+    <th>Status</th>
+    <th>Actions</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>1</td>
+    <td>Mark</td>
+    <td>Otto</td>
+    <td>@mdo</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Jacob</td>
+    <td>Thornton</td>
+    <td>@fat</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td colSpan={2}>Larry the Bird</td>
+    <td>@twitter</td>
+  </tr>
+</tbody>
+</Table>
+
+
       ))}
     </div>
   );
